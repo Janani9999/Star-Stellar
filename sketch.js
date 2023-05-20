5379381fe4a8b1ba58900c841b87ac678b55b4a2
Janani9999 <108892354+Janani9999@users.noreply.github.com>
2023-05-20 11:12:12 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var splashimg
+
+
+function preload(){
+splashimg=loadImage("Star_Stellar.gif")
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight)
+
+}
+
+function draw(){
+background(splashimg)
+    
+}
